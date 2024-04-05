a796273c730f1379ce3d1c812f293a92c4e5d082
Sahibzada-A <wary-hop9i@icloud.com>
2024-04-05 13:13:33 +0100

    Define type interfaces

---
+++ Agentic-Accounting-Firm/packages/shared/src/types/index.ts
@@ -0,0 +1,21 @@
+export * from './common';
+export * from './metadata';
+export * from './commands';
+export type {
+  Account,
+  Transaction,
+  TransactionQuery,
+  TransactionCreateInput,
+  TransactionUpdateInput,
+  TransactionCategorization,
+  ChartOfAccount,
+  JournalEntry,
+  Budget,
+  Report,
+  ReportType,
+  ReportConfig,
+  TaxRate,
+  TransactionFilters,
+  AccountingService,
+  AccountingAdapter,
+} from './accounting';
